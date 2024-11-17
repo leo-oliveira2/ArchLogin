d036a4a5d6684fe3161d3a4ee649c77f7da03981
lh-oliveira2 <leonardo.heinen04@edu.pucrs.br>
2024-11-17 00:15:08 -0300

    (fix) - namespace added into Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
+namespace ArchLogin;
 using StackExchange.Redis;
 using System;
 using System.Threading.Tasks;
 
-namespace ReferenceConsoleRedisApp
+
+class Program
 {
-  class Program
+  static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis-11137.c336.samerica-east1-1.gce.redns.redis-cloud.com:11137,password=*******");
+  static async Task Main(string[] args)
   {
-    static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis-11137.c336.samerica-east1-1.gce.redns.redis-cloud.com:11137,password=*******");
-    static async Task Main(string[] args)
-    {
-      var db = redis.GetDatabase();
-    }
+    var db = redis.GetDatabase();
   }
 }
