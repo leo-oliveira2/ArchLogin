5af546d07a21502cd0d71d7d2837ecc3efea163b
lh-oliveira2 <leonardo.heinen04@edu.pucrs.br>
2024-11-16 23:59:26 -0300

    (fix) - created shared AssemblyInfo.cs and included into the .csproj files; (add) - created the Redis connection.

---
+++ common/SharedAssembyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("ArchLogin")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622440e71510465778bec22619fc86c56a28f410")]
+[assembly: System.Reflection.AssemblyProductAttribute("ArchLogin")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ArchLogin")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
